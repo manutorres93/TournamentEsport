296d79976dca2b39d534512c69545d2509398d2d
manutorres93 <manutorres9312@gmail.com>
2024-06-04 07:54:37 -0400

    Persitence done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,39 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigType } from '@nestjs/config';
+import dbConfig from './persistence/db-config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      load: [dbConfig],
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigType<typeof dbConfig>) => {
+        const { db } = configService;
+
+        return {
+          type: 'mysql',
+          host: db.host,
+          port: db.port,
+          username: db.user,
+          password: db.password,
+          database: db.name,
+          entities: ['dist/**/*.entity{.ts,.js}'],
+          autoLoadEntities: true,
+          synchronize: true,
+          retryDelay:3000,
+          retryAttempts:10 // Esto no debería estar en producción
+          
+        };
+      },
+      inject: [dbConfig.KEY],}),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
